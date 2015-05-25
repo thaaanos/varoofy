fcbc87b96c6fd10a61a9dffd9bfb64751698c267
Mark Menkhus <mark.menkhus@gmail.com>
2015-05-25 09:30:59 -0600

    added a setup.py, removed the env virtualenv from project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+"""
+setup(name='miner',
+              py_modules = ['', 'module2']
+              packages = ['package1', 'package2']
+              scripts = ['script1', 'script2'])
+"""
+setup(name='miner',
+     py_modules = ['miner','site'],
+     packages = ['miner'])
